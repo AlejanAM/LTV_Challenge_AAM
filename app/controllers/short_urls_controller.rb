b30112996d42c53f2b35d7353d08dcf7fe1d1e04
Alejandro Acuña Morera <moreraalejandro@estudiantec.cr>
2022-10-31 17:41:52 -0600

    Top 100 URL's, Short Code and missing comments
    Feature of getting the top 100 url's and creation of shortcode based on id added

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -3,22 +3,34 @@ class ShortUrlsController < ApplicationController
   # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
 
+  #Returns the Url's based on the amount of clicks
+  #with a limit of 100
   def index
+    result = ShortUrl.limit(100).order('click_count desc')
+    render json:{
+      urls: result.map{
+        |code|code.short_code
+      },
+      status: 200
+    }
+
+
   end
 
   #Takes the URLS in string format, checks
   #if there are any errors and executes the
   #post on the database
   def create
-    crURL = ShortUrl.new(full_url:params[:full_url])
+    @crURL = ShortUrl.new(full_url:params[:full_url])
     if crURL.save
-      render json: crURL, status: 200
+      render json: {short_code: @crURL.short_code}, status: 200
     else
-      render json: {error:crURL.errors}
+      render json: @crURL.errors
     end
   end
 
   def show
+
   end
 
 end
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -4,7 +4,24 @@ class ShortUrl < ApplicationRecord
 
   validate :validate_full_url
 
+
+  #With the URL id in table it converts the
+  #decimal id to base 62 number and with
+  #each each digit it finds the base 62
   def short_code
+    key = self.id
+    str = ""
+    while key > 0
+      str += CHARACTERS[key % 62]
+      key = (key/62).floor
+    end
+    str.reverse
+  end
+
+  #Returns the actual shortcode
+  #for the URL when asked
+  def public_attributes
+    self.short_code
   end
 
   def update_title!
@@ -13,15 +30,15 @@ class ShortUrl < ApplicationRecord
   private
 
 
-  #Validates if there is a host on the URL
+  #Validates the existence of a host on the URL
   #and only the sites with security (HTTPS)
   def validate_full_url
     begin
       uri = URI.parse(full_url)
     rescue URI::InvalidURIError
     end
-    if !uri.host && uri.kind_of?(URI::HTTPS)
-      errors.add(:errors,"Invalid URL")
+    if !uri.host && !uri.kind_of?(URI::HTTPS)
+      errors.add(:errors,"Full url is not a valid url")
     end
   end
  
